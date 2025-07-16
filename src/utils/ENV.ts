265a76263ac3f11811f7d98291363cee2b7c195e
Jefry Agustin Astacio Sanchez <jesanchez@DCTIC.GC>
2025-07-16 07:48:57 -0400

    Arreglado

--- src/utils/ENV.ts
+++ src/utils/ENV.ts
@@ -1,4 +1,4 @@
 // export const URL_API = `http://192.168.100.65:10000`;
-export const URL_API = `http://172.20.10.2:10000`;
+export const URL_API = `http://192.168.55.53:1000`;
 // export const URL_API = `http://192.168.53.54:3000`;
 // export const URL_API = `https://server2-o3im.onrender.com`;
